56aed5db38fdc798a19e6ca1b484159bd68de1c5
German Sanchez <german.sanchez@prodigious.com>
2021-04-30 16:27:49 -0500

    fisrt react counter app

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value = 10 }) => {
+
+    const [ counter, setCounter ] = useState( value );
+
+    // handleAdd
+    const handleAdd = (e) => {
+        // setCounter( counter + 1); // other way to do it
+        setCounter( (c) => c + 1  );
+    }
+    // handleSubstract
+    const handleSubstract = () => setCounter( counter - 1); 
+
+    // handleReset
+    const handleReset = () => setCounter( value );
+
+
+    return(
+        <>
+            <h1>CounterApp</h1>
+            <h2>{ counter }</h2>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
+        </>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number,
+}
+
+export default CounterApp;
---
+++ 03-counter-app/src/FirstApp.js
@@ -0,0 +1,28 @@
+// import React, { Fragment } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+
+const FirstApp = ({ greet, subtitle }) => {
+  // const person =  {
+  //     name: 'Andres',
+  //     age: 24,
+  // }
+
+  return (
+    <>
+      <h1> {greet} </h1>
+      {/* <pre> { JSON.stringify( person, null, 3) } </pre> */}
+      <p>{subtitle}</p>
+    </>
+  );
+};
+
+FirstApp.propTypes = {
+  greet: PropTypes.string.isRequired,
+};
+
+FirstApp.defaultProps = {
+    subtitle: 'I am a subtitle'
+}
+
+export default FirstApp;
