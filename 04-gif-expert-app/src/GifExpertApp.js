9c414e838d5f0ee2434114baa86cf42fc035cd61
German Sanchez <german.sanchez@prodigious.com>
2021-05-12 07:27:42 -0500

    Gif expert App build

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
+
+    
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories( [...categories, 'The Walking Dead'] );
+    //     setCategories( cate => [...cate, 'The Walking Dead'] );
+    // }
+
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory  setCategories={ setCategories }/>
             <hr/>
+
+
+            <ol>
+                { 
+                    categories.map( category => 
+                        <GifGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    )
+                }
+            </ol>
         </>
     )
 }
