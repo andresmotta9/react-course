25f650b81bc2b3a0ad3b25531af25b86d11aeda0
German Sanchez <german.sanchez@prodigious.com>
2021-06-01 17:40:53 -0500

    Tests on Private route

--- 06-heroes-app/src/tests/routers/PrivateRoute.test.js
+++ 06-heroes-app/src/tests/routers/PrivateRoute.test.js
@@ -29,6 +29,23 @@ describe('Tests on <PrivateRoute />', () => {
         expect ( wrapper.find('span').exists() ).toBe(true);
         expect( localStorage.setItem ).toHaveBeenCalledWith("lastPath",'/marvel');
 
+    });
+
+    test('should block the component if ther user is not autheticated', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute 
+                    isAuthenticated={ false }
+                    component={ () => <span>Ready</span> }
+                    { ...props }
+                />
+            </MemoryRouter>
+        );
+
+        expect ( wrapper.find('span').exists() ).toBe(false);
+        expect( localStorage.setItem ).toHaveBeenCalledWith("lastPath",'/marvel');
+        
     })
+    
     
 })
